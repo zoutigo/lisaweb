3286d260cfe3760aa5f9de041c750ec89247ef04
zoutigo <zoutigo@gmail.com>
2025-12-20 11:53:31 +0100

    fix static assets

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const path = require("path");
 
-// App root = directory of this file
 const appRoot = __dirname;
-
-// Passenger can start in a different CWD -> force it
-process.chdir(appRoot);
+const standaloneDir = path.join(appRoot, ".next", "standalone");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
-// cPanel/Passenger provides PORT, fallback just in case
 process.env.PORT = process.env.PORT || "3000";
 
-// Launch Next.js standalone server
-require(path.join(appRoot, ".next", "standalone", "server.js"));
+// Run from the standalone directory
+process.chdir(standaloneDir);
+
+require(path.join(standaloneDir, "server.js"));
