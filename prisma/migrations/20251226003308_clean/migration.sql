4c5928d87b6306167f1d4afe9f71916d5bcb613f
zoutigo <zoutigo@gmail.com>
2025-12-26 01:59:08 +0100

    features: formats

--- prisma/migrations/20251226003308_clean/migration.sql
+++ prisma/migrations/20251226003308_clean/migration.sql
@@ -10,14 +10,3 @@ ALTER TABLE `Partner` ALTER COLUMN `id` DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE `Rendezvous` ALTER COLUMN `id` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `ServiceOffer` MODIFY `shortDescription` VARCHAR(191) NOT NULL,
-    MODIFY `longDescription` VARCHAR(191) NOT NULL,
-    ALTER COLUMN `updatedAt` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `ServiceOfferStep` MODIFY `description` VARCHAR(191) NOT NULL;
-
--- AlterTable
-ALTER TABLE `ServiceOfferUseCase` MODIFY `description` VARCHAR(191) NOT NULL;
